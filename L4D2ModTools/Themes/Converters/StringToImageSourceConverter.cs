790131d30306e6ffa8ccc4358e6424316d06d0d6
穗乃果 <1763575579@qq.com>
2023-01-07 02:40:51 +0800

    update

--- L4D2ModTools/Themes/Converters/StringToImageSourceConverter.cs
+++ L4D2ModTools/Themes/Converters/StringToImageSourceConverter.cs
@@ -5,7 +5,7 @@ public class StringToImageSourceConverter : IValueConverter
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         var path = (string)value;
-        if (!string.IsNullOrEmpty(path))
+        if (File.Exists(path))
             return new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
         else
             return null;
---
+++ TestDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TestDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("==========  程序开始!  ==========");
+        Console.WriteLine();
+
+        Console.WriteLine("hello");
+
+        Console.WriteLine();
+        Console.WriteLine("==========  程序结束!  ==========");
+        Console.ReadLine();
+    }
+}
