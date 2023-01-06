02c5854a1b1ad3f7185a809e3238509eee3b7fb8
穗乃果 <1763575579@qq.com>
2023-01-06 23:33:18 +0800

    fix bug

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleAppTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var iPAddress = IPAddress.Parse("127.0.0.1");
+
+        //Console.WriteLine(iPAddress.Address);
+        Console.WriteLine(iPAddress.ToString());
+
+        Console.WriteLine(iPAddress.GetHashCode());
+        var addressBytes = iPAddress.GetAddressBytes();
+        Console.WriteLine(BitConverter.ToUInt32(addressBytes));
+
+        var iPAddress2 = new IPAddress(16777343);
+        Console.WriteLine(iPAddress2);
+
+        Console.WriteLine("程序结束!");
+        Console.ReadLine();
+    }
+}
--- Steamworks/Utility/Platform.cs
+++ Steamworks/Utility/Platform.cs
@@ -8,20 +8,12 @@ using System.Text;
 
 namespace Steamworks
 {
-	internal static class Platform
+    internal static class Platform
     {
-#if PLATFORM_WIN64
-		public const int StructPlatformPackSize = 8;
-		public const string LibraryName = "steam_api64";
-#elif PLATFORM_WIN32
-		public const int StructPlatformPackSize = 8;
-		public const string LibraryName = "steam_api";
-#elif PLATFORM_POSIX
-		public const int StructPlatformPackSize = 4;
-		public const string LibraryName = "libsteam_api";
-#endif
+        public const int StructPlatformPackSize = 8;
+        public const string LibraryName = "steam_api";
 
-		public const CallingConvention CC = CallingConvention.Cdecl;
-		public const int StructPackSize = 4;
-	}
+        public const CallingConvention CC = CallingConvention.Cdecl;
+        public const int StructPackSize = 4;
+    }
 }
