0c469339bb4fdde88326b4bdafe93adb23a20d26
穗乃果 <1763575579@qq.com>
2023-01-10 18:40:09 +0800

    update

--- L4D2ModTools/Properties/AssemblyInfo.cs
+++ L4D2ModTools/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyFileVersion("1.0.0.4")]
 // 不重复
 [assembly: AssemblyDescription("https://github.com/CrazyZhang666/L4D2ModTools")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © 2022-2023")]
